fb08d30bf66e5abc820ef60d9811a887b77f90bf
Vanshika-1 <124459030+Vanshika-1@users.noreply.github.com>
2023-03-18 01:14:21 +0530

    Update main.py
    Imported Libraries and function to define database

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
 
+#import libraries
+from tkinter import *
+import tkinter.ttk as ttk
+import tkinter.messagebox as tkMessageBox
+import sqlite3
+
+#function to define database
+def Database():
+    global conn, cursor
+    #creating student database
+    conn = sqlite3.connect("student.db")
+    cursor = conn.cursor()
+    #creating STUD_REGISTRATION table
+    cursor.execute(
+        "CREATE TABLE IF NOT EXISTS STUD_REGISTRATION (STU_ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, STU_NAME TEXT, STU_CONTACT TEXT, STU_EMAIL TEXT, STU_ROLLNO TEXT, STU_BRANCH TEXT)")
+    
